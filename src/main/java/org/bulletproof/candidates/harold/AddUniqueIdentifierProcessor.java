ffddcd410863824865aa77943d7c97c33831f37f
Harold <Harold@Harold-PC>
2015-05-05 20:16:34 +1000

    Added more descriptive javadoc comments at class level

--- src/main/java/org/bulletproof/candidates/harold/AddUniqueIdentifierProcessor.java
+++ src/main/java/org/bulletproof/candidates/harold/AddUniqueIdentifierProcessor.java
@@ -3,7 +3,11 @@ package org.bulletproof.candidates.harold;
 import org.springframework.batch.item.ItemProcessor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.DependsOn;
-
+/**
+ * This class does transform each row of the file by appending a unique identifier to the data.
+ * @author Harold
+ *
+ */
 @DependsOn("counterBean")
 public class AddUniqueIdentifierProcessor implements
 		ItemProcessor<CSVRowData, CSVRowData> {
--- src/main/java/org/bulletproof/candidates/harold/AutoIncrementService.java
+++ src/main/java/org/bulletproof/candidates/harold/AutoIncrementService.java
@@ -3,7 +3,7 @@ package org.bulletproof.candidates.harold;
 import org.springframework.stereotype.Service;
 
 /**
- * Contract with the auto increment service component.
+ * Contract with the auto increment service component, a default implementation for this class is provided via configuration at {@link org.bulletproof.candidates.harold.BatchConfiguration#DefaultIncrementService()}
  * @author Harold
  *
  */
